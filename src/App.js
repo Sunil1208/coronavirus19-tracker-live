2371a6142934524b37e119074f7bcfec2a456c2b
Sunil1208 <skmy03@gmail.com>
2020-04-16 00:11:38 +0530

    Card done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import styles from './App.module.css'
 import { fetchData } from './api'
 
 class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            data : {},
+        }
+    }
     
     async componentDidMount(){
-        const data = await fetchData();
-        console.log(data)
+        const fetchedData = await fetchData();
+        console.log(fetchedData)
+
+        this.setState({
+            data: fetchedData
+        });
     }
     render(){
+        const {data} = this.state;
         return(
             <div className={styles.container}>
-                <Cards/>
+                <Cards data={data}/>
                 <CountryPicker/>
                 <Chart/>
             </div>
--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,9 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get(url);
-        
-        //console.log(response);
-        return response;
+        const {data :{ confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);   //destructuring the data
+            
+        return { confirmed, recovered, deaths, lastUpdate};
         
     } catch (error) {
         
